c2888d0e554afe7466c0f66a2a3b36c4368fbfd5
yuraikitsuki <akbar.rizki@student.unri.ac.id>
2017-01-17 13:20:36 +0700

    excecutivesummary.php
    ini sudah fix. no typo

---
+++ executivesummary.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Executive Summary</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body bgcolor="#D0FA58">
+
+<div class="container">
+  <div class="container">
+  <div class="jumbotron">
+    <h1>Executive Summary</h1>      
+    <h3>SPR Langgak have a summary of the business plan : </h3><br>
+  </div> 
+</div>
+
+  <div class="media">
+    <div class="media-left media-top">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">First</h4>
+      <p>The Province of Riau offers security stability that benefit the businesses, local and foreign investors, as reflected it is the home of World Class Organizations</p>
+    </div>
+  </div>
+  <hr>
+  <div class="media">
+    <div class="media-left media-middle">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Second</h4>
+      <p>The Province of Riau offers security stability that benefit the businesses, local and foreign investors, as reflected it is the home of World Class Organizations</p>
+    </div>
+  </div>
+  <hr>
+  <div class="media">
+    <div class="media-left media-bottom">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Third</h4>
+      <p>The Interests, given as Production Sharing Contract (“PSC”),  was awarded by the Government of Indonesia in November 2009, which became effective on April 20, 2010</p>
+    </div>
+  </div>
+  <hr>
+  <div class="media">
+    <div class="media-left media-bottom">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Fourth</h4>
+      <p>As the Operator, SPRL responsibles in operating of Langgak Field, that include Production, Financing and Marketing activities related to the Field</p>
+    </div>
+  </div>
+  <hr>
+  <div class="media">
+    <div class="media-left media-bottom">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Fifth</h4>
+      <p>Langgak Field (“the Field”) shared the infrastructures costs with CPI for the use of electrical supply and transportational pipeline including the use of Gathering Station to Dumai Port</p>
+    </div>
+  </div>
+  <hr>
+  <div class="media">
+    <div class="media-left media-bottom">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Sixth</h4>
+      <p>SPRL commits to increase the production level of the Field to 700 barrel per day at the end of 2010, by the use of advanced technologies in oil services maintenance, work over and enhanced oil recovery</p>
+    </div>
+  </div>
+  <hr>
+  <div class="media">
+    <div class="media-left media-bottom">
+      <img src="logohd/Logo HD.png" class="media-object" style="width:80px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Seventh</h4>
+      <p>Infrastructures in Riau is ready to handling oil exports from Dumai Ports</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
