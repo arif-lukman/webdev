aa5c69418f56db2e66a6fc8af4dbd5e719533c11
yuraikitsuki <akbar.rizki@student.unri.ac.id>
2017-01-17 11:04:50 +0700

    on board manager1
    masih belum lengkap fotonya, ntar di update lagi pas karyawan ini kasi
    fotonya sama foto versi HD

---
+++ obmanager.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>On Board Manager</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      position: relative; 
+  }
+  #section1 {padding-top:50px;height:500px;color: #fff; background-color: #1E88E5;}
+  #section2 {padding-top:50px;height:500px;color: #fff; background-color: #673ab7;}
+  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #ff9800;}
+  #section4 {padding-top:50px;height:500px;color: #fff; background-color: #00bcd4;}
+  #section5 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
+  #section6 {padding-top:50px;height:500px;color: #fff; background-color: #BF00FF;}
+  #section7 {padding-top:50px;height:500px;color: #fff; background-color: #0B610B;}
+  </style>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php">SPR Langgak</a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="#section1">SPRL Director</a></li>
+          <li><a href="#section2">SPRL Business Support Manager</a></li>
+          <li><a href="#section3">SPRL E&D Manager</a></li>
+          <li><a href="#section4">SPRL Finance Manager</a></li>
+		  <li><a href="#section5">SPRL Production & Operation Manager</a></li>
+		  <li><a href="#section6">Head of SCM</a></li>
+		  <li><a href="#section7">Head of HSSE</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>    
+
+<div id="section1" class="container-fluid">
+  <h1><br>SPRL Director<br></h1>
+  <div class="container">          
+  <img src="fotomanager/ik.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----Ikin Faizal----<h2>
+</div>
+</div>
+<div id="section2" class="container-fluid">
+  <h1><br>SPRL Business Support Manager<br></h1>
+  <div class="container"> 
+  <img src="fotomanager/mah.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----Mahpuzoh----<h2>
+  </div>
+</div>
+<div id="section3" class="container-fluid">
+  <h1><br>SPRL E&D Manager<br></h1>
+  <div class="container"> 
+  <img src="fotomanager/har.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----Hardianto----<h2>
+  </div>
+</div>
+<div id="section4" class="container-fluid">
+  <h1><br>SPRL Finance Manager<br></h1>
+  <div class="container"> 
+  <img src="fotomanager/ar.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----Arif Wicaksono----<h2>
+  </div>
+</div>
+<div id="section5" class="container-fluid">
+  <h1><br>SPRL Production & Operation Manager<br></h1>
+  <div class="container"> 
+  <img src="fotomanager/yas.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----M. Yasin Abdulfatah----<h2>
+  </div>
+</div>
+<div id="section6" class="container-fluid">
+  <h1><br>Head of SCM<br></h1>
+  <div class="container"> 
+  <img src="fotomanager/.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----Susi Kardina Ria----<h2>
+  </div>
+</div>
+<div id="section7" class="container-fluid">
+  <h1><br>Head of HSSE<br></h1>
+  <div class="container"> 
+  <img src="fotomanager/.jpg" class="img-circle" alt="image" width="304" height="236"> 
+  <h2>----Hardi----<h2>
+  </div>
+</div>
+</body>
+</html>
