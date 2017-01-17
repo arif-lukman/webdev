36105296b1ed9b4cd01ce74f2102691ddb46963f
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-01-17 15:49:20 +0700

    obmanager fixed

--- obmanager.php
+++ obmanager.php
@@ -1,86 +1,140 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-  <title>On Board Manager</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-  body {
-      position: relative; 
-  }
-  #section1 {padding-top:50px;height:500px; }
-  #section2 {padding-top:50px;height:500px; }
-  #section3 {padding-top:50px;height:500px; }
-  #section4 {padding-top:50px;height:500px; }
-  #section5 {padding-top:50px;height:500px; }
-  #section6 {padding-top:50px;height:500px; }
-  #section7 {padding-top:50px;height:500px; }
-  
-    h1, h2, h3, h4, h5, h6 {
-        text-align: center;
-    }
-  </style>
-</head>
-<body>
- 
-<div>
-  <h1>SPRL Director</h1>
-  <div class="container"> 
-  <img src="fotomanager/ik.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----Ikin Faizal----</h2>
-  </div>
-</div>
-<hr>
-<div>
-  <h1>SPRL Business Support Manager</h1>
-  <div class="container"> 
-  <img src="fotomanager/mah.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----Mahpuzoh----</h2>
-  </div>
-</div>
-<hr>
-<div>
-  <h1>SPRL E&D Manager</h1>
-  <div class="container"> 
-  <img src="fotomanager/har.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----Hardianto----</h2>
-  </div>
-</div>
-<hr>
-<div>
-  <h1>SPRL Finance Manager</h1>
-  <div class="container"> 
-  <img src="fotomanager/ar.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----Arif Wicaksono----</h2>
-  </div>
-</div>
-<hr>
-<div>
-  <h1>SPRL Production & Operation Manager</h1>
-  <div class="container"> 
-  <img src="fotomanager/yas.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----M. Yasin Abdulfatah----</h2>
-  </div>
-</div>
-<hr>
-<div>
-  <h1>Head of SCM</h1>
-  <div class="container"> 
-  <img src="fotomanager/.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----Susi Kardina Ria----</h2>
-  </div>
-</div>
-<hr>
-<div>
-  <h1>Head of HSSE</h1>
-  <div class="container"> 
-  <img src="fotomanager/.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
-  <h2>----Hardi----</h2>
-  </div>
-</div>
-<hr>
-</body>
+	<head>
+		<!--Override css-->
+		<style>
+			#map {
+				height: 400px;
+				width: 100%;
+			}
+			.motto{
+				font-size: 30px;
+				font-style: italic;
+			}
+			.grad{
+				background-color: #FFDF00;
+				border-color: transparent;
+			}
+			.white{
+				background: white;
+			}
+			a{
+				color: white;
+			}
+			.container{
+				max-width: 787px;
+			}
+
+			#section1 {padding-top:50px;height:500px; }
+		  	#section2 {padding-top:50px;height:500px; }
+		  	#section3 {padding-top:50px;height:500px; }
+		  	#section4 {padding-top:50px;height:500px; }
+		  	#section5 {padding-top:50px;height:500px; }
+		  	#section6 {padding-top:50px;height:500px; }
+		  	#section7 {padding-top:50px;height:500px; }
+			  
+		    h1, h2, h3, h4, h5, h6 {
+		        text-align: center;
+		    }
+		</style>
+		<title>SPR Langgak</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<div class="container">
+			<center><img src="logospr.png"></center>
+			<div class="col-sm-12 text-center motto">
+				Providing world's energy. Today
+			</div>
+		</div>
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<ul class="nav navbar-nav">
+				<li><a href="index.php">Home</a></li>
+				<li class="dropdown active">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">About SPR Langgak
+					<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="profile.php">Profile</a></li>
+						<li><a href="executivesummary.php">Executive Summary</a></li>
+						<li><a href="visimisi.php">Vision & Mission</a></li>
+						<li><a href="langgakfield.php">Langgak Field</a></li>
+						<li><a href="msgdir.php">Message From Director</a></li>
+						<li class="active"><a href="obmanager.php">Onboard Manager</a></li>
+					</ul>
+				</li>
+				<li><a href="news.php">News & Events</a></li>
+				<li><a href="procurement.php">Procurement</a></li>
+				<li><a href="production.php">Production</a></li>
+				<li><a href="reports.php">Reports</a></li>
+				<li><a href="contactus.php">Contact Us</a></li>
+				</ul>
+			</nav>
+			<div>
+			  <h1>SPRL Director</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/ik.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----Ikin Faizal----</h2>
+			  </div>
+			</div>
+			<hr>
+			<div>
+			  <h1>SPRL Business Support Manager</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/mah.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----Mahpuzoh----</h2>
+			  </div>
+			</div>
+			<hr>
+			<div>
+			  <h1>SPRL E&D Manager</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/har.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----Hardianto----</h2>
+			  </div>
+			</div>
+			<hr>
+			<div>
+			  <h1>SPRL Finance Manager</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/ar.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----Arif Wicaksono----</h2>
+			  </div>
+			</div>
+			<hr>
+			<div>
+			  <h1>SPRL Production & Operation Manager</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/yas.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----M. Yasin Abdulfatah----</h2>
+			  </div>
+			</div>
+			<hr>
+			<div>
+			  <h1>Head of SCM</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----Susi Kardina Ria----</h2>
+			  </div>
+			</div>
+			<hr>
+			<div>
+			  <h1>Head of HSSE</h1>
+			  <div class="container"> 
+			  <img src="fotomanager/.jpg" class="img-circle img-responsive center-block" alt="image" width="100" height="100"> 
+			  <h2>----Hardi----</h2>
+			  </div>
+			</div>
+			<hr>
+		</div>
+	</body>
+
 </html>
