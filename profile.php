361687a8fcae8515d3efa099efa79a81cb36f9bd
yuraikitsuki <akbar.rizki@student.unri.ac.id>
2017-01-17 09:12:59 +0700

    profile
    ini untuk profle

---
+++ profile.php
@@ -0,0 +1,75 @@
+<div class="jumbotron text-center">
+  <h1>Profile SPR Langak</h1>
+  <p>about SPR Langgak</p> 
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Image Gallery</h2>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="foto\langgak.png" target="_blank">
+          <img src="foto\langgak.png" alt="Langgak Gathering" style="width:100%">
+          <div class="caption">
+            <p>Langgak Gathering Station</p>
+          </div>
+        </a>
+      </div>
+    </div>
+	
+	
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="foto\lapangan.png" target="_blank">
+          <img src="foto\lapangan.png" alt="Nature" style="width:100%">
+          <div class="caption">
+            <p>Production Operation</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="foto\watertank.png" target="_blank">
+          <img src="foto\watertank.png" alt="Fjords" style="width:100%">
+          <div class="caption">
+            <p>Water Tank</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <h2>About SPR Langgak</h2>
+  <div class="well">SPRL commits to increase the production level of the Field to 700 barrel per day at the end of 2010, by the use of advanced technologies in oil services maintenance, work over and enhanced oil recovery.
+SPRL commits to increase the production level of the Field to 700 barrel per day at the end of 2010, by the use of advanced technologies in oil services maintenance, work over and enhanced oil recoverySPRL commits to increase the production level of the Field to 700 barrel per day at the end of 2010, by the use of advanced technologies in oil services maintenance, work over and enhanced oil recoverySPRL commits to increase the production level of the Field to 700 barrel per day at the end of 2010, by the use of advanced technologies in oil services maintenance, work over and enhanced oil recovery
+</div>
+</div>
+
+<div class="container">
+  <h2>PT Sarana Pembangunan Riau</h2>
+  <ul class="list-group">
+    <li class="list-group-item">1. PT Sarana Pembangunan Riau (“SPR”) holds fifty percent of Participating Interests in the Field</li>
+    <li class="list-group-item">2. The Interests, given as Production Sharing Contract (“PSC”),  was awarded by the Government of Indonesia in November 2009, which became effective on April 20, 2010</li>
+    <li class="list-group-item">3. Holds 99.9% of ownerships in SPRL, SPR fully supported financially for the developement of the Field</li>
+	<li class="list-group-item">4. Established in 1990, SPR is the oldest Province of Riau’s owned company in Riau</li>
+	<li class="list-group-item">5. SPR repsonsibles in managing commercial assets of the Province such as Gas Turbine Generator, Landbanks and Heavy Equipment.</li>
+  </ul>
+</div>
+
+</body>
+</html>
