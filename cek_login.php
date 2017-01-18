dbc887ffa4074318dc1820f6162d9032cb9cb361
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-01-18 15:32:54 +0700

    admin login page added + very simple login mechanism, users table creation sql added

---
+++ cek_login.php
@@ -0,0 +1,21 @@
+<?php
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+	$query = mysql_query("select * from users where username='$username' and password='$password' or email='$username' and password='$password'");
+	$count = mysql_num_rows($query);
+	$row = mysql_fetch_array($query);
+
+	if($count == 1)
+		{
+			header ("location:main_menu.php?active_id=" . $row["id_user"]);
+		}
+		else
+		{
+			echo "<script> alert('Username atau Password Salah');
+			location='admin.php';
+			</script>";
+		}
+?>
