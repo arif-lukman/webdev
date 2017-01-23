6d81b6f6bd151e6bff5484b3a12a0acc16bded99
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-01-23 10:56:21 +0700

    excel file download feature added

---
+++ download.php
@@ -0,0 +1,21 @@
+<?php
+	include "koneksi.php";
+
+	$filename = "tes.xls";
+
+	//Download file
+	header("Content-Disposition: attachment; filename=\"$filename\"");
+	header("Content-Type: application/vnd.ms-excel");
+	$query = mysql_query("SELECT * FROM sot_sprl");
+
+	//Write data to file
+	$flag = false;
+	while($row = mysql_fetch_assoc($query)){
+		if(!$flag){
+			//display field/column names as first row
+			echo implode("\t", array_keys($row)) . "\r\n";
+			$flag = true;
+		}
+		echo implode("\t", array_values($row)) . "\r\n";
+	}
+?>
