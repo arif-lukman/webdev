0521176e1732ca07986607b1f058e6f51627d26f
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-01-30 15:19:11 +0700

    minor change

--- news-admin/postAcceptor.php
+++ news-admin/postAcceptor.php
@@ -7,7 +7,7 @@
   /*********************************************
    * Change this line to set the upload folder *
    *********************************************/
-  $imageFolder = "uploads/";
+  $imageFolder = "../assets/images/uploads/";
 
   reset ($_FILES);
   $temp = current($_FILES);
