7c216ff4cf5dd58b452c0668598db3ea2b32a804
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-01-31 15:31:01 +0700

    step 13-15

---
+++ cooperation/step13.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Step 13</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Go to step
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="step1.php">Step 1 : Nama dan Tipe Perusahaan</a></li>
+      <li><a href="step2.php">Step 2 : Partner K3S</a></li>
+      <li><a href="step3.php">Step 3 : Alamat Kantor / Office Address</a></li>
+	  <li><a href="step4.php">Step 4 : Susunan pengurus / Struktur organisasi</a></li>
+	  <li><a href="step5.php">Step 5 : Daftar Pemilik / Shareholders</a></li>
+	  <li><a href="step6.php">Step 6 : Dokumen Administrasi / Administration Document</a></li>
+	  <li><a href="step7.php">Step 7 : Surat Keagenan / Dealer / Distributor</a></li>
+	  <li><a href="step8.php">Step 8 : Daftar Rekening Bank Perusahaan / Office Bank Accounts</a></li>
+	  <li><a href="step9.php">Step 9 : Perusahaan Induk, Grup Perusahaan, Rekanan, Konsorsium, Afiliasi dan Aliansi</a></li>
+	  <li><a href="step10.php">Step 10 : Pengalaman Perusahaan / Company Experience</a></li>
+	  <li><a href="step11.php">Step 11 : Perusahaan Pembuat Barang / Good Manufacturer</a></li>
+	  <li><a href="step12.php">Step 12 : Klasifikasi Perusahaan / Company Classification</a></li>
+	  <li><a href="step13.php">Step 13 : Keadaan Perusahaan / Company Subject</a></li>
+	  <li><a href="step14.php">Step 14 : Surat dan Dokumen Pelengkap / Supporting Documents</a></li>
+	  <li><a href="step15.php">Step 15 : Pengajuan / Submission</a></li>
+    </ul>
+  </div>
+  
+<div class="col-sm-2"></div>
+			<form class="col-sm-8">
+				<h2>Step 13</h2>
+				<h3>Nama dan Tipe Perusahaan</h3>
+				<hr>
+					 <div class="well well-lg">
+			
+				<div class="form-group">
+			  		<label for="name">1. Proses bangkrut(Bankruptcy proceeding)</label>
+				  	<label class="radio-inline"><input type="radio" name="opt1">Yes</label>
+					<label class="radio-inline"><input type="radio" name="opt1">No</label>
+				</div>
+				<div class="form-group">
+			  		<label for="name">2. Pengawasan pengadilan(Court supervision)</label>
+				  	<label class="radio-inline"><input type="radio" name="opt2">Yes</label>
+					<label class="radio-inline"><input type="radio" name="opt2">No</label>
+				</div>
+				<div class="form-group">
+			  		<label for="name">3. Kegiatan usaha sedang dihentikan(Suspension of business activities)</label>
+				  	<label class="radio-inline"><input type="radio" name="opt3">Yes</label>
+					<label class="radio-inline"><input type="radio" name="opt3">No</label>
+				</div>
+				<div class="form-group">
+			  		<label for="name">4. Tuntutan atau claim dari pihak ketiga atau pemerintah(Claims or suity with third partes including government agencies)</label>
+				  	<label class="radio-inline"><input type="radio" name="opt4">Yes</label>
+					<label class="radio-inline"><input type="radio" name="opt4">No</label>
+				</div>
+				<div class="form-group">
+			  		<label for="name">5. Sedang dikenakan sanksi hukum berdasarkan undang-undang kriminal(Is being sanctioned by criminal law)</label>
+				  	<label class="radio-inline"><input type="radio" name="opt5">Yes</label>
+					<label class="radio-inline"><input type="radio" name="opt5">No</label>
+				</div>
+				<div class="form-group">
+			  		<label for="name">6. Sedang dikenakan sanksi oleh perusahaan K3S atau perusahaan migas lainnya(Is being sanctioned by K3S or other oil company)</label>
+				  	<label class="radio-inline"><input type="radio" name="opt6">Yes</label>
+					<label class="radio-inline"><input type="radio" name="opt6">No</label>
+				</div>
+
+				
+<button type="button" class="btn btn-primary">Save</button>
+<button type="button" class="btn btn-primary">Reset</button>
+<hr>
+  <ul class="pager">
+    <li><a href="step12.php">Previous Step</a></li>
+    <li><a href="step14.php">Next Step</a></li>
+  </ul>
+  
+			</form>
+		</div>
+
+</body>
+</html>		
