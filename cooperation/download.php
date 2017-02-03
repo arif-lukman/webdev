9be03d65ddd2501965ffe3d454d1ee0a58b12089
yuraikitsuki <akbar.rizki@student.unri.ac.id>
2017-02-03 09:56:20 +0700

    bantuan added.. fiture read and download....

--- cooperation/download.php
+++ cooperation/download.php
@@ -1,26 +1,38 @@
-<?php
-// Source Code Download File dengan PHP
-if(isset($_GET['bantuan.pdf'])){
- $file = $back_dir.$_GET['bantuan.pdf'];
- 
- if (file_exists($file))
- {
-  header('Content-Description: File Transfer');
-  header('Content-Type: application/octet-stream');
-  header('Content-Disposition: attachment; filename='.basename($file));
-  header('Content-Transfer-Encoding: binary');
-  header('Expires: 0');
-  header('Cache-Control: private');
-  header('Pragma: private');
-  header('Content-Length: ' . filesize($file));
-  ob_clean();
-  flush();
-  readfile($file);
-  exit;
- } 
- else 
- {
-  echo "file {$_GET['bantuan.pdf']} sudah tidak ada.";
- }
-}
-?>
+<!doctype html>
+<html>
+
+<head><title></title></head>
+<html>
+	<head>
+		<title>Ganti Password</title>
+
+		<!--override css-->
+		<link rel="stylesheet" type="text/css" href="../assets/css/stylebook.css">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+		<style>
+					a.home {
+				position: fixed;
+				top: 0;
+				right: 0;
+				color: white;
+			}
+		</style>
+	</head>
+	
+<body>
+
+<html><center><embed src="bantuan.pdf" type="application/pdf" width="850" height="590"/></center>
+
+<a class="home" href="vendor.php"><img src="../assets/images/cooperation/back.png" height="50" width="100"></a>
+</body>
+
+</html>
