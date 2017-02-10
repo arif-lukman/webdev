54bf2fc4d4b6171b34ab14556bbb1079f5b878d9
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-02-10 13:28:49 +0700

    dumping data buat db admin

--- assets/external files/_bpms_master.sql
+++ assets/external files/_bpms_master.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 02 Feb 2017 pada 08.45
+-- Generation Time: 10 Feb 2017 pada 13.27
 -- Versi Server: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -42,7 +42,9 @@ CREATE TABLE `_admin` (
 --
 
 INSERT INTO `_admin` (`_id`, `_username`, `_fullname`, `_email`, `_password`, `_group_id`, `_status`, `_desc`) VALUES
-(1, 'admin', 'Administrator', 'admin@gmail.com', 'sprl1234', '1', 1, 'ini admin, ini form, ugh admin fom');
+(1, 'admin', 'Administrator', 'admin@gmail.com', 'as', '1', 1, 'ini admin, ini form, ugh admin form'),
+(3, 'kos', 'kos', 'kos@kos.com', 'kos', '4', 0, '1'),
+(4, 'arif', 'arif', 'arif@yahoo.com', 'arif', '1', 1, 'arif');
 
 -- --------------------------------------------------------
 
@@ -54,9 +56,19 @@ CREATE TABLE `_affil_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_affil_type`
+--
+
+INSERT INTO `_affil_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'PI', 'Perusahaan Induk', 1, 1),
+(2, 'GP', 'Grup Perusahaan', 2, 1),
+(3, 'RK', 'Rekanan', 3, 1),
+(4, 'KNS', 'Konsorsium', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -68,9 +80,18 @@ CREATE TABLE `_bank` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_nama` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_bank`
+--
+
+INSERT INTO `_bank` (`_id`, `_kode`, `_nama`, `_order`, `_status`) VALUES
+(1, 'BNI', 'Bank Negara Indonesia', 1, 1),
+(2, 'BRI', 'Bank Rakyat Indonesia', 2, 1),
+(3, 'DAN', 'Danamon', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -82,9 +103,19 @@ CREATE TABLE `_class_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_kelas` text NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_class_type`
+--
+
+INSERT INTO `_class_type` (`_id`, `_kode`, `_judul`, `_order`, `_kelas`, `_status`) VALUES
+(1, 'A01', 'Ekplorasi, Produksi dan Pengolangan Lanjutan', 1, 'Bidang', 1),
+(2, 'A0101', 'Peralatan/suku cadang pemboran, eksplorasi dan produksi', 2, 'Sub Bidang', 1),
+(3, 'A02', 'Kelas 2', 3, 'Bidang', 1);
 
 -- --------------------------------------------------------
 
@@ -96,9 +127,19 @@ CREATE TABLE `_company_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_company_type`
+--
+
+INSERT INTO `_company_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'PT', 'Perseroan Terbatas', 1, 1),
+(2, 'CV', 'Persekutuan Komanditer', 2, 1),
+(3, 'Koperasi', 'Koperasi', 3, 1),
+(4, 'Lembaga', 'Lembaga', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -110,9 +151,18 @@ CREATE TABLE `_country` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_nama` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_country`
+--
+
+INSERT INTO `_country` (`_id`, `_kode`, `_nama`, `_order`, `_status`) VALUES
+(1, 'INA', 'Indonesia', 1, 1),
+(2, 'MLY', 'Malaysia', 2, 1),
+(3, 'USA', 'Amerika', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -124,37 +174,64 @@ CREATE TABLE `_currency` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_nama` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_currency`
+--
+
+INSERT INTO `_currency` (`_id`, `_kode`, `_nama`, `_order`, `_status`) VALUES
+(1, 'IDR', 'Rupiah', 1, 1),
+(2, 'MYR', 'Ringgit', 2, 1),
+(3, 'USD', 'Dollar Amerika', 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `_dist_type`
+-- Struktur dari tabel `_distributor`
 --
 
-CREATE TABLE `_dist_type` (
+CREATE TABLE `_distributor` (
   `_id` int(11) NOT NULL,
-  `_kode` text NOT NULL,
-  `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_nama` text NOT NULL,
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_distributor`
+--
+
+INSERT INTO `_distributor` (`_id`, `_nama`, `_order`, `_status`) VALUES
+(1, 'Agen Tunggal', 1, 1),
+(2, 'Agen', 2, 1),
+(3, 'Agen Ganda', 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `_document+_type`
+-- Struktur dari tabel `_docplus_type`
 --
 
-CREATE TABLE `_document+_type` (
+CREATE TABLE `_docplus_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_docplus_type`
+--
+
+INSERT INTO `_docplus_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'SMK3', 'SMK3', 1, 1),
+(2, 'Jamsostek', 'JMS', 2, 1),
+(3, 'SILO', 'SILO', 3, 1),
+(4, 'TBH', 'Tambahan', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -164,11 +241,21 @@ CREATE TABLE `_document+_type` (
 
 CREATE TABLE `_document_type` (
   `_id` int(11) NOT NULL,
-  `_kode` int(11) NOT NULL,
-  `_judul` int(11) NOT NULL,
+  `_kode` text NOT NULL,
+  `_judul` text NOT NULL,
   `_order` int(11) NOT NULL,
-  `_status` int(11) NOT NULL
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_document_type`
+--
+
+INSERT INTO `_document_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'AKT', 'Akte Pendirian Perusahaan (AKT)', 1, 1),
+(2, 'SK-MKH', 'SK Menteri Kehakiman dan HAM (SK-MKH)', 2, 1),
+(3, 'SIUP', 'Surat Ijin Usaha Perusahaan (SIUP)', 3, 1),
+(4, 'MENG', 'Dokumen Biasa', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -188,14 +275,23 @@ CREATE TABLE `_expiry_cfg` (
 --
 
 CREATE TABLE `_general_cfg` (
+  `_id` int(11) NOT NULL,
   `_nama_app` text NOT NULL,
   `_desc` text NOT NULL,
   `_email_1` text NOT NULL,
   `_email_2` text NOT NULL,
   `_email_proc` text NOT NULL,
   `_ttd_skt` text NOT NULL,
-  `_footer_txt` text NOT NULL
+  `_footer_txt` text NOT NULL,
+  `_img` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_general_cfg`
+--
+
+INSERT INTO `_general_cfg` (`_id`, `_nama_app`, `_desc`, `_email_1`, `_email_2`, `_email_proc`, `_ttd_skt`, `_footer_txt`, `_img`) VALUES
+(1, 'aksjdadknl', 'hggiugiiha', 'oihoh@gmail.com', 'asep@gmail.com', 'asep@gmail.com', 'ajshdahdosh', 'ashdoahsdoh', 'Wisata Alam.png');
 
 -- --------------------------------------------------------
 
@@ -218,7 +314,11 @@ CREATE TABLE `_group_priv` (
 --
 
 INSERT INTO `_group_priv` (`_id`, `_name`, `_view`, `_add`, `_edit`, `_delete`, `_setting`) VALUES
-(1, 'Admin', 1, 1, 1, 1, 1);
+(1, 'Admin', 1, 1, 1, 1, 1),
+(2, 'Data Entry', 1, 1, 0, 0, 0),
+(3, 'Viewer', 1, 0, 0, 0, 0),
+(4, 'Editor', 1, 0, 1, 0, 0),
+(5, 'Procurement', 1, 0, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -230,9 +330,18 @@ CREATE TABLE `_manager_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_manager_type`
+--
+
+INSERT INTO `_manager_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'BOD', 'Dewan Direksi', 1, 1),
+(2, 'BOC', 'Dewan Komisaris', 2, 1),
+(3, 'BOM', 'DUARRR', 3, 0);
 
 -- --------------------------------------------------------
 
@@ -244,9 +353,19 @@ CREATE TABLE `_office_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_office_type`
+--
+
+INSERT INTO `_office_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'KP', 'Kantor Pusat', 1, 1),
+(2, 'BO', 'Kantor Cabang', 2, 1),
+(3, 'RO', 'Kantor Perwakilan', 3, 1),
+(4, 'SO', 'Special Operator', 4, 0);
 
 -- --------------------------------------------------------
 
@@ -258,9 +377,18 @@ CREATE TABLE `_province` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_nama` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_province`
+--
+
+INSERT INTO `_province` (`_id`, `_kode`, `_nama`, `_order`, `_status`) VALUES
+(1, 'ID-AC', 'Aceh', 1, 1),
+(2, 'ID-BA', 'Bali', 2, 1),
+(3, 'HB-HC', 'Jawa Utara', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -271,9 +399,18 @@ CREATE TABLE `_province` (
 CREATE TABLE `_qual_type` (
   `_id` int(11) NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_qual_type`
+--
+
+INSERT INTO `_qual_type` (`_id`, `_judul`, `_order`, `_status`) VALUES
+(1, 'Besar', 1, 1),
+(2, 'Kecil', 3, 1),
+(3, 'Menengah', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -284,8 +421,15 @@ CREATE TABLE `_qual_type` (
 CREATE TABLE `_regist_cfg` (
   `_judul` text NOT NULL,
   `_desc` text NOT NULL,
-  `_status` text NOT NULL
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_regist_cfg`
+--
+
+INSERT INTO `_regist_cfg` (`_judul`, `_desc`, `_status`) VALUES
+('asdasda', 'asdasdads', 0);
 
 -- --------------------------------------------------------
 
@@ -297,9 +441,39 @@ CREATE TABLE `_scope_type` (
   `_id` int(11) NOT NULL,
   `_kode` text NOT NULL,
   `_judul` text NOT NULL,
-  `_order` text NOT NULL,
-  `_status` text NOT NULL
+  `_order` int(11) NOT NULL,
+  `_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_scope_type`
+--
+
+INSERT INTO `_scope_type` (`_id`, `_kode`, `_judul`, `_order`, `_status`) VALUES
+(1, 'PB', 'Pengadaan Barang', 1, 1),
+(2, 'JS', 'Jasa Pemborongan', 2, 1),
+(3, 'JK', 'Jasa Konsultasi', 3, 1),
+(4, 'JL', 'Jasa Lainnya', 4, 1),
+(5, 'JCS', 'Judul', 5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `_status`
+--
+
+CREATE TABLE `_status` (
+  `_id` int(11) NOT NULL,
+  `_nama` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `_status`
+--
+
+INSERT INTO `_status` (`_id`, `_nama`) VALUES
+(0, 'Inactive'),
+(1, 'Active');
 
 --
 -- Indexes for dumped tables
@@ -348,21 +522,27 @@ ALTER TABLE `_currency`
   ADD PRIMARY KEY (`_id`);
 
 --
--- Indexes for table `_dist_type`
+-- Indexes for table `_distributor`
 --
-ALTER TABLE `_dist_type`
+ALTER TABLE `_distributor`
   ADD PRIMARY KEY (`_id`);
 
 --
--- Indexes for table `_document+_type`
+-- Indexes for table `_docplus_type`
 --
-ALTER TABLE `_document+_type`
+ALTER TABLE `_docplus_type`
   ADD PRIMARY KEY (`_id`);
 
 --
 -- Indexes for table `_document_type`
 --
 ALTER TABLE `_document_type`
+  ADD PRIMARY KEY (`_id`);
+
+--
+-- Indexes for table `_general_cfg`
+--
+ALTER TABLE `_general_cfg`
   ADD PRIMARY KEY (`_id`);
 
 --
@@ -402,6 +582,12 @@ ALTER TABLE `_scope_type`
   ADD PRIMARY KEY (`_id`);
 
 --
+-- Indexes for table `_status`
+--
+ALTER TABLE `_status`
+  ADD PRIMARY KEY (`_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -409,82 +595,87 @@ ALTER TABLE `_scope_type`
 -- AUTO_INCREMENT for table `_admin`
 --
 ALTER TABLE `_admin`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `_affil_type`
 --
 ALTER TABLE `_affil_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `_bank`
 --
 ALTER TABLE `_bank`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `_class_type`
 --
 ALTER TABLE `_class_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `_company_type`
 --
 ALTER TABLE `_company_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `_country`
 --
 ALTER TABLE `_country`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `_currency`
 --
 ALTER TABLE `_currency`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `_dist_type`
+-- AUTO_INCREMENT for table `_distributor`
 --
-ALTER TABLE `_dist_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `_distributor`
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `_document+_type`
+-- AUTO_INCREMENT for table `_docplus_type`
 --
-ALTER TABLE `_document+_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `_docplus_type`
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `_document_type`
 --
 ALTER TABLE `_document_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `_general_cfg`
+--
+ALTER TABLE `_general_cfg`
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `_group_priv`
 --
 ALTER TABLE `_group_priv`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `_manager_type`
 --
 ALTER TABLE `_manager_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `_office_type`
 --
 ALTER TABLE `_office_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `_province`
 --
 ALTER TABLE `_province`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `_qual_type`
 --
 ALTER TABLE `_qual_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `_scope_type`
 --
 ALTER TABLE `_scope_type`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
