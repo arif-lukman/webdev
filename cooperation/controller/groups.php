74444206ed4568273cdd4ccda84255fbe45e365e
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-02-14 15:00:30 +0700

    group privileges page minor edit

--- cooperation/controller/groups.php
+++ cooperation/controller/groups.php
@@ -22,14 +22,6 @@
 	$input = array();
 	array_push($input, $admin, $daten, $viewer, $editor, $proc);
 
-	print_r($input);
-	//print_r($admin);
-	//print_r($daten);
-	//print_r($viewer);
-	//print_r($editor);
-	//print_r($proc);
-	//$sql = "UPDATE _group_priv SET WHERE";
-
 	//update tabel
 	for($i = 0; $i<sizeof($default); $i++){
 		$sql = "update _group_priv set _view=" . $input[$i][0] . ", _add=" . $input[$i][1] . ", _edit=" . $input[$i][2] . ", _delete=" . $input[$i][3] . ", _setting=" . $input[$i][4] . " where _id=" . ($i+1);
