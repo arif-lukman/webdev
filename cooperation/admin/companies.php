d3a5f48ae05b76044b6d8cf40e7749a39c67597a
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-02-16 11:03:01 +0700

    admin pages edit

--- cooperation/admin/companies.php
+++ cooperation/admin/companies.php
@@ -1,79 +1,39 @@
+<?php
+	//include library
+	include "../lib/library.php";
+
+	//set variabel nama db
+	$dbname = "_bpms_master";
+
+	//include file koneksi
+	include "../controller/koneksi.php";
+?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>SPR Langgak</title>
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<?php
+			//inisialisasi head
+			initHead();
+		?>
 	</head>
 
 	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-				<a class="navbar-brand" href="#">SPRL BPMS</a>
-				</div>
-				<ul class="nav navbar-nav">
-					<li><a href="main_menu.php">Registrasi</a></li>
-					<li class="active"><a href="companies.php">Data Perusahaan</a></li>
-					<li><a href="proposals.php">Status Pengajuan</a></li>
-					<li><a href="expiry.php">Kadaluarsa</a></li>
-					<li><a href="cfg_gen.php">Admin</a></li>
-					<li><a href="#">Bantuan</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="index.php">Logout</a></li>
-				</ul>
-			</div>
-		</nav>
+		<?php
+			//bikin navbarnya
+			createNavbar(setActiveNav(NAVBAR, "companies.php"));
+		?>
 		<div class="container" style="margin-top: 80px">
 			<div class="well">
-				<form action="search_comp.php">
-					<div class="form-group">
-						<label for="keyword">Kata kunci:</label>
-						<input type="text" class="form-control" id="keyword" name="keyword">
-					</div>
-					<div class="form-group">
-						<label for="country">Negara:</label>
-						<select class="form-control" id="country" name="country">
-						    <option>Indonesia</option>
-						    <option>Malaysia</option>
-							<option>Singapura</option>
-							<option>Amerika</option>
-							<option>Cina</option>
-							<option>Inggris</option>
-							<option>Russia</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="province">Propinsi:</label>
-						<select class="form-control" id="country" name="country">
-
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="class">Klasifikasi:</label>
-						<input type="text" class="form-control" id="class" name="class">
-					</div>
-					<div class="form-group">
-						<label for="qualific">Kualifikasi Perusahaan:</label>
-						<select class="form-control" id="qualific" name="qualific">
-						    <option>Indonesia</option>
-						    <option>Malaysia</option>
-							<option>Singapura</option>
-							<option>Amerika</option>
-							<option>Cina</option>
-							<option>Inggris</option>
-							<option>Russia</option>
-						</select>
-					</div>
-					<input type="submit" value="search">
+				<h3>Data Perusahaan</h3><hr>
+				<form action="search_comp.php" method="post">
+					<?php
+						echo createInputField("text", "Kata kunci:", "keyword", "keyword", "");
+						echo createSelectOption("Negara:", "country", "country", $conn, "SELECT _id, _nama as _name FROM _country");
+						echo createSelectOption("Propinsi:", "province", "province", $conn, "SELECT _id, _nama as _name FROM _province");
+						echo createSelectOption("Klasifikasi:", "class", "class", $conn, "SELECT _id, _judul as _name FROM _class_type");
+						echo createSelectOption("Kualifikasi:", "qual", "qual", $conn, "SELECT _id, _judul as _name FROM _qual_type");
+					?>
+					<input type="submit" value="Search">
 				</form>
 			</div>
 		</div>
--- cooperation/admin/expiry.php
+++ cooperation/admin/expiry.php
@@ -1,69 +1,37 @@
+<?php
+	//include library
+	include "../lib/library.php";
+
+	//set variabel nama db
+	$dbname = "_bpms_master";
+
+	//include file koneksi
+	include "../controller/koneksi.php";
+?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>SPR Langgak</title>
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<?php
+			//inisialisasi head
+			initHead();
+		?>
 	</head>
 
 	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-				<a class="navbar-brand" href="#">SPRL BPMS</a>
-				</div>
-				<ul class="nav navbar-nav">
-					<li><a href="main_menu.php">Registrasi</a></li>
-					<li><a href="companies.php">Data Perusahaan</a></li>
-					<li><a href="proposals.php">Status Pengajuan</a></li>
-					<li class="active"><a href="expiry.php">Kadaluarsa</a></li>
-					<li><a href="cfg_gen.php">Admin</a></li>
-					<li><a href="#">Bantuan</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="index.php">Logout</a></li>
-				</ul>
-			</div>
-		</nav>
+		<?php
+			//bikin navbarnya
+			createNavbar(setActiveNav(NAVBAR, "expiry.php"));
+		?>
 		<div class="container" style="margin-top: 80px">
 			<div class="well">
-				<form action="search_expi.php">
-					<div class="form-group">
-						<label for="keyword">Kata kunci:</label>
-						<input type="text" class="form-control" id="keyword" name="keyword">
-					</div>
-					<div class="form-group">
-						<label for="status">Status Kadaluarsa:</label>
-						<select class="form-control" id="status" name="status">
-						    <option>Indonesia</option>
-						    <option>Malaysia</option>
-							<option>Singapura</option>
-							<option>Amerika</option>
-							<option>Cina</option>
-							<option>Inggris</option>
-							<option>Russia</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="type">Tipe Dokumen:</label>
-						<select class="form-control" id="type" name="type">
-						    <option>Indonesia</option>
-						    <option>Malaysia</option>
-							<option>Singapura</option>
-							<option>Amerika</option>
-							<option>Cina</option>
-							<option>Inggris</option>
-							<option>Russia</option>
-						</select>
-					</div>
-					<input type="submit" value="search">
+				<h3>Dokumen Kadaluarsa</h3><hr>
+				<form action="search_stat.php">
+					<?php
+						echo createInputField("text", "Kata kunci:", "keyword", "keyword", "");
+						echo createSelectOption("Negara:", "country", "country", $conn, "SELECT _id, _nama as _name FROM _country");
+						echo createSelectOption("Propinsi:", "province", "province", $conn, "SELECT _id, _nama as _name FROM _province");
+					?>
+					<input type="submit" value="Search">
 				</form>
 			</div>
 		</div>
