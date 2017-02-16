71fb7480eb934ef18b66826c142b0fb7d4e28cb5
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-02-16 11:03:20 +0700

    admin pages edit 2

--- cooperation/admin/main_menu.php
+++ cooperation/admin/main_menu.php
@@ -1,84 +1,50 @@
+<?php
+	//include library
+	include "../lib/library.php";
+
+	//set variabel nama db
+	$dbname = "_bpms_master";
+
+	//include file koneksi
+	include "../controller/koneksi.php";
+
+	//data untuk select option negara
+	//ambil nama field
+	$fieldNames1 = getResults("SHOW columns FROM _document_type WHERE field = '_id' || field = '_kode' || field = '_judul' || field = '_kadaluarsa'", $conn);
+
+	//ambil isi field
+	$fieldValues1 = getResults("SELECT _id, _kode, _judul, _kadaluarsa FROM _document_type ORDER BY _id ASC", $conn);
+
+	//push isi field ke array
+	$allValues1 = pushArray($fieldValues1);
+?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>SPR Langgak</title>
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<?php
+			//inisialisasi head
+			initHead();
+		?>
 	</head>
 
 	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-				<a class="navbar-brand" href="#">SPRL BPMS</a>
-				</div>
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="main_menu.php">Registrasi</a></li>
-					<li><a href="companies.php">Data Perusahaan</a></li>
-					<li><a href="proposals.php">Status Pengajuan</a></li>
-					<li><a href="expiry.php">Kadaluarsa</a></li>
-					<li><a href="cfg_gen.php">Admin</a></li>
-					<li><a href="#">Bantuan</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="index.php">Logout</a></li>
-				</ul>
-			</div>
-		</nav>
+		<?php
+			//bikin navbarnya
+			createNavbar(setActiveNav(NAVBAR, "main_menu.php"));
+		?>
 		<div class="container" style="margin-top: 80px">
 			<div class="well">
+				<h3>Registrasi</h3><hr>
 				<form action="controller/register_company.php" method="post">
-					<div class="form-group">
-						<label for="name">Username:</label>
-						<input type="text" class="form-control" id="name" name="username">
-					</div>
-					<div class="form-group">
-						<label for="company">Nama Perusahaan:</label>
-						<input type="text" class="form-control" id="company" name="company">
-					</div>
-					<div class="form-group">
-						<label for="country">Negara:</label>
-						<select class="form-control" id="country" name="country">
-						    <option>Indonesia</option>
-						    <option>Malaysia</option>
-							<option>Singapura</option>
-							<option>Amerika</option>
-							<option>Cina</option>
-							<option>Inggris</option>
-							<option>Russia</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="province">Propinsi:</label>
-						<select class="form-control" id="province" name="province">
-							<option>Indonesia</option>
-						    <option>Malaysia</option>
-							<option>Singapura</option>
-							<option>Amerika</option>
-							<option>Cina</option>
-							<option>Inggris</option>
-							<option>Russia</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="email">Email:</label>
-						<input type="email" class="form-control" id="email" name="email">
-					</div>
-					<div class="form-group">
-						<label for="password">Kata Sandi:</label>
-						<input type="password" class="form-control" id="password" name="password">
-					</div>
-					<div class="form-group">
-						<label for="desc">Keterangan:</label>
-						<textarea class="form-control" rows="5" id="desc" name="desc"></textarea>
-					</div>
+					<?php
+						echo createInputField("text", "Username:", "uname", "uname", "");
+						echo createInputField("text", "Nama Perusahaan:", "cpname", "cpname", "");
+						echo createSelectOption("Negara:", "country", "country", $conn, "SELECT _id, _nama as _name FROM _country");
+						echo createSelectOption("Propinsi:", "province", "province", $conn, "SELECT _id, _nama as _name FROM _province");
+						echo createInputField("email", "Email:", "email", "email", "");
+						echo createInputField("password", "Kata Sandi:", "password", "password", "");
+						echo createTextArea(3, "Keterangan:", "desc", "desc", "");
+					?>
 					<input type="submit" value="Create">
 				</form>
 			</div>
