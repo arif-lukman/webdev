ef7259217232cc171b6d48c3b7995ad9521cc20d
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-02-16 07:37:35 +0700

    document expiration config page edit

--- cooperation/cfg_exp.php
+++ cooperation/cfg_exp.php
@@ -1,6 +1,21 @@
 <?php
 	//include library
 	include "lib/library.php";
+
+	//set variabel nama db
+	$dbname = "_bpms_master";
+
+	//include file koneksi
+	include "controller/koneksi.php";
+
+	//ambil nama field
+	$fieldNames = getResults("SHOW columns FROM _document_type WHERE field = '_id' || field = '_kode' || field = '_judul' || field = '_kadaluarsa'", $conn);
+
+	//ambil isi field
+	$fieldValues = getResults("SELECT _id, _kode, _judul, _kadaluarsa FROM _document_type ORDER BY _id ASC", $conn);
+
+	//push isi field ke array
+	$allValues = pushArray($fieldValues);
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,17 +39,16 @@
 			<div class="well col-sm-9">
 				<h3>Setting Masa Kadaluarsa</h3><hr>
 				<form>
-					<div class="form-group">
-				  		<label for="doc">Admin Document:</label>
-					  	<select class="form-control" id="doc">
-						    <option>Active</option>
-						    <option>Inactive</option>
-						  </select>
-					</div>
 					<?php
 						//bikin field pada form
+						echo createSelectOption("Admin Document:", "admin", "admin", $conn, "SELECT _id, _judul as _name FROM _document_type ORDER BY _order ASC");
 						echo createInputField("text", "Masa Kadaluarsa:", "judul", "judul", "");
-						echo createTextArea(3, "Deskripsi:", "desc", "desc", "");
+					?>
+					<button type="submit" class="btn btn-default">Save</button>
+					<br>
+					<?php
+						//bikin tabel buat nampilin semua datanya
+						generateTable($fieldNames, $fieldValues, $allValues, "typ_doc.php");
 					?>
 				</form>
 			</div>
